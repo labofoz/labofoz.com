5361d10f2731cf92ac6c49f5b75e803320aa5d4a
Oz Ramos <heyozramos@gmail.com>
2019-08-15 14:05:41 -0700

    Autologins and show username/avatar

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
 </template>
 
 <script>
+import { pick } from "lodash";
 import * as firebase from "firebase/app";
 import "firebase/auth";
 import firebaseConfig from "../firebase.config.json";
@@ -15,25 +16,25 @@ const provider = new firebase.auth.GoogleAuthProvider();
 export default {
   name: "App",
 
+  /**
+   * Handle autologin/out
+   */
   mounted() {
     window.app = this;
     this.$root.$on("firebaseLogin", this.firebaseLogin);
+    firebase.auth().onAuthStateChanged(user => this.onFirebaseAuthChange(user));
   },
 
   methods: {
+    /**
+     * Manually log into Firebase
+     */
     firebaseLogin() {
       this.$store.commit("set", ["isBusy.app", true]);
 
       firebase
         .auth()
         .signInWithPopup(provider)
-        .then(result => {
-          let user = result.user;
-          this.$q.notify({
-            message: `Welcome back ${user.displayName}`,
-            color: "positive"
-          });
-        })
         .catch(error => {
           let errorCode = error.code;
           let errorMessage = error.message;
@@ -46,6 +47,29 @@ export default {
         .finally(() => {
           this.$store.commit("set", ["isBusy.app", false]);
         });
+    },
+
+    /**
+     * Handle login and auto
+     */
+    onFirebaseAuthChange(user) {
+      if (user) {
+        this.$store.commit("set", [
+          "user",
+          pick(user, ["displayName", "photoURL", "uid"])
+        ]);
+
+        this.$q.notify({
+          message: `Welcome back ${user.displayName}`,
+          avatar: user.photoURL,
+          color: "positive"
+        });
+      } else {
+        this.$q.notify({
+          message: `You have been logged out`,
+          color: "positive"
+        });
+      }
     }
   }
 };
--- src/store/index.js
+++ src/store/index.js
@@ -23,7 +23,10 @@ export default function(/* { ssrContext } */) {
     state: {
       isBusy: {
         app: false
-      }
+      },
+
+      // Firebase user data
+      user: null
     },
 
     mutations: {
